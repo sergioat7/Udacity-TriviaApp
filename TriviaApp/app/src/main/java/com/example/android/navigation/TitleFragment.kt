b9aff9903d2d20deec8bfd287f122e31c572c571
Sergio Aragonés <sergioat7@gmail.com>
2021-05-30 16:55:26 +0200

    Created navigation graph

--- TriviaApp/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ TriviaApp/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -11,6 +11,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
@@ -22,6 +23,7 @@ class TitleFragment : Fragment() {
         // Inflate the layout for this fragment
         val binding: FragmentTitleBinding =
             DataBindingUtil.inflate(layoutInflater, R.layout.fragment_title, container, false)
+        binding.playButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment))
         return binding.root
     }
 }
