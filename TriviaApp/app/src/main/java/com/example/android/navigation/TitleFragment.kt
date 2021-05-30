29c81b5d8cdb880dfe76d640627f24366db28db6
Sergio Aragonés <sergioat7@gmail.com>
2021-05-30 18:15:30 +0200

    Added menu to navigate to About fragment

--- TriviaApp/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ TriviaApp/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -6,12 +6,12 @@
 package com.example.android.navigation
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
@@ -24,6 +24,19 @@ class TitleFragment : Fragment() {
         val binding: FragmentTitleBinding =
             DataBindingUtil.inflate(layoutInflater, R.layout.fragment_title, container, false)
         binding.playButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment))
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.overflow_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(
+            item,
+            findNavController()
+        ) || super.onOptionsItemSelected(item)
     }
 }
